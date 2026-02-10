6e770ed1404e9a6556849b5e30ae29d47353619e
Lee-seungmihn <tmdals8277@gmail.com>
2026-02-10 17:51:52 +0900

    memberVO 작업

---
+++ src/main/java/com/app/ggshop/v1/domain/MemberVO.java
@@ -0,0 +1,20 @@
+package com.app.ggshop.v1.domain;
+
+import com.app.ggshop.v1.audit.dateTime.Period;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter @ToString
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+//@SuperBuilder
+public class MemberVO extends Period {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+    private String memberNickname;
+    private String memberBirth;
+    private String memberAddress;
+}
